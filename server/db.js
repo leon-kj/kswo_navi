7d5d8d1882b07995fd225b90d949f242ae19c6a3
Beliona <125764062+Beliona@users.noreply.github.com>
2025-01-11 23:11:13 +0100

    Quick change to hide password

--- server/db.js
+++ server/db.js
@@ -1,10 +1,15 @@
 const { Pool } = require('pg'); // Import the 'pg' module
+const fs = require('fs'); // Import the 'fs' module for reading files
+
+// Read the password from the file
+const password = fs.readFileSync('server/password.txt', 'utf8');
+
 
 const pool = new Pool({
     user: 'postgres',            
     host: 'localhost',           
     database: 'kswo_navi_rooms',     
-    password: 'leonleon',   
+    password: password,   
     port: 5432,                  // Default PostgreSQL port
 });
 

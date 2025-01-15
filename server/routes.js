a6b7470621247c80a9ec2171bb050773f0b4b86e
Beliona <125764062+Beliona@users.noreply.github.com>
2025-01-15 20:12:52 +0100

    mS Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -17,8 +17,14 @@ router.get('/api/rooms', async (req, res) => { // Get all rooms
 router.get('/api/rooms/:name', async (req, res) => { // Get room by name
     try {
         const { name } = req.params;
-        const result = await pool.query('SELECT id, name, floor, ST_AsGeoJSON(geom) AS geom, metadata, type, teacher FROM rooms WHERE name = $1 OR UPPER(type) = $1', [name]);
-        res.json(result.rows[0]);
+        const result = await pool.query(`
+          SELECT id, name, floor, ST_AsGeoJSON(geom) AS geom, metadata, type, teacher 
+          FROM rooms 
+          WHERE name LIKE $1 OR UPPER(type) LIKE $1`,
+           [`%${name}%`]
+          );
+
+        res.json(result.rows);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
@@ -27,7 +33,12 @@ router.get('/api/rooms/:name', async (req, res) => { // Get room by name
 router.get('/api/rooms/floor/:floor', async (req, res) => { // Get rooms by floor
     try {
         const { floor } = req.params;
-        const result = await pool.query('SELECT id, name, floor, ST_AsGeoJSON(geom) AS geom, metadata, type, teacher FROM rooms WHERE floor = $1', [floor]);
+        const result = await pool.query(`
+          SELECT id, name, floor, ST_AsGeoJSON(geom) AS geom, metadata, type, teacher 
+          FROM rooms 
+          WHERE floor = $1`, 
+          [floor]);
+
         res.json(result.rows);
     } catch (err) {
         console.error(err.message);
